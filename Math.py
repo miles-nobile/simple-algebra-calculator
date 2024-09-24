560e5e8cb115192d60b728f3d4bcde7e6094fbb0
Miles Nobile <milesnobile@gmail.com>
2024-09-24 18:48:58 -0400

    pairSort

--- Math.py
+++ Math.py
@@ -1,10 +1,48 @@
-global x
-x = 0
+
+
+def pairSort(pair = "0,0",pair1 = "0,0"):
+    global x
+    global y
+    global x1
+    global y1
+    try:
+        pairReplace = pair.replace("(", "")
+        pairReplace = pairReplace.replace(")", "")
+        pairReplace = pairReplace.replace(" ", "")
+        x,y = pairReplace.split(",")
+        x = float(x)
+        y = float(y)
+
+        pairReplace = pair1.replace("(", "")
+        pairReplace = pairReplace.replace(")", "")
+        pairReplace = pairReplace.replace(" ", "")
+        x1,y1 = pairReplace.split(",")
+        x1 = float(x1)
+        y1 = float(y1)
+
+    except:
+        print("Please type in valid ordered pairs like (2,8) or -4,0")
+        test()
+        success = False
+
 
 def test():
-    x = input("x = ")
-    print(x * 1)
+    # used so the calculations aren't repeated
+    global success
+    success = True
+    # gets info
+    pair = input("Point = ")
+    rize = input("Rize = ")
+    run = input("Run = ")
+
+    pairSort(pair)
+
+    if success:
+        print(f"x = {x} y = {y}")
+    success = True
 
 test()
+
+
 
 
