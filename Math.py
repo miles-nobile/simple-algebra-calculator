4164a0aa4f297f9ef8a4b629aab26cc589d879d6
Miles Nobile <milesnobile@gmail.com>
2024-09-24 20:02:48 -0400

    slopeFind

--- Math.py
+++ Math.py
@@ -5,6 +5,7 @@ def pairSort(pair = "0,0",pair1 = "0,0"):
     global y
     global x1
     global y1
+    global pairSuccess
     try:
         pairReplace = pair.replace("(", "")
         pairReplace = pairReplace.replace(")", "")
@@ -19,27 +20,55 @@ def pairSort(pair = "0,0",pair1 = "0,0"):
         x1,y1 = pairReplace.split(",")
         x1 = float(x1)
         y1 = float(y1)
+        pairSuccess = True
 
     except:
         print("Please type in valid ordered pairs like (2,8) or -4,0")
         test()
+        pairSuccess = False
+
+def slopeFind(rize = 0 ,run = 1):
+    global slope
+    global success
+    try:
+        rize = float(rize)
+        run = float(run)
+        slope = rize/run
+        success = True
+    except ZeroDivisionError:
+        print("run can't be 0")
         success = False
+        test()
+    except:
+        print("Please type in valid rize and run like rize = 2 and run = 1")
+        test()
+        success = False
+
+
+
+
 
 
 def test():
     # used so the calculations aren't repeated
+    global pairSuccess
     global success
-    success = True
+    success = False
+    pairSuccess = False
+
     # gets info
     pair = input("Point = ")
-    rize = input("Rize = ")
-    run = input("Run = ")
-
     pairSort(pair)
 
-    if success:
+    if pairSuccess:
+        rize = input("Rize = ")
+        run = input("Run = ")
+        slopeFind(rize,run)
+
+    if pairSuccess and success:
         print(f"x = {x} y = {y}")
-    success = True
+
+    pairSuccess = True
 
 test()
 
