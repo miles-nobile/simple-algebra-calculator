dee8738387d1cc04c4679c8307eb68c360aafcba
Miles Nobile <milesnobile@gmail.com>
2024-10-04 21:46:40 -0400

    gui

---
+++ GUImath.py
@@ -0,0 +1,297 @@
+from fractions import Fraction
+
+global solved
+
+def findSlopeFromPointEquation():
+    global Slope
+    global printSlope
+    global rize
+    global run
+    rize = y1 - y
+    run = x1 - x
+    Slope = rize / run
+    if not Slope.is_integer():
+        printSlope = Fraction(Slope).limit_denominator()
+
+
+def standardEquation(a, b, c):
+    if b > 0:
+        print(f"{a}x +{b}y = {c}")
+    elif b < 0:
+        print(f"{a}x {b}y = {c}")
+
+
+def pointSlopeEquation(slope, yIntercept, equals="y"):
+    if yIntercept > 0 and slope.is_integer() and yIntercept.is_integer():
+        print(f"{equals} = {int(slope)}x +{int(yIntercept)}")
+    elif yIntercept == 0.0 and slope.is_integer():
+        print(f"{equals} = {int(slope)}x")
+    elif yIntercept < 0.0 and slope.is_integer() and yIntercept.is_integer():
+        print(f"{equals} = {int(slope)}x {int(yIntercept)}")
+    elif yIntercept > 0 and not slope.is_integer() and yIntercept.is_integer():
+        print(f"{equals} = {Fraction(slope).limit_denominator()}x +{int(yIntercept)}")
+    elif yIntercept == 0.0 and not slope.is_integer():
+        print(f"{equals} = {Fraction(slope).limit_denominator()}x")
+    elif yIntercept < 0.0 and not slope.is_integer() and yIntercept.is_integer():
+        print(f"{equals} = {Fraction(slope).limit_denominator()}x {int(yIntercept)}")
+    elif yIntercept > 0 and not slope.is_integer() and not yIntercept.is_integer():
+        print(f"{equals} = {Fraction(slope).limit_denominator()}x +{Fraction(yIntercept).limit_denominator()}")
+    elif yIntercept < 0.0 and not slope.is_integer() and not yIntercept.is_integer():
+        print(f"{equals} = {Fraction(slope).limit_denominator()}x {Fraction(yIntercept).limit_denominator()}")
+
+
+def xToYConverter(programNumber=1, slope1=0.0, yIntercept1=0.0, ):
+    global fOfx
+    run = True
+    print("Type stop to exit\n")
+    if programNumber == 1:
+        fOfxIn = input("f(x) = ")
+    while run:
+        try:
+            xIn = input("x = ")
+            if xIn == "stop":
+                run = False
+                programNumber = 0
+
+            else:
+                xIn = float(xIn)
+
+            if programNumber == 0:
+                print("good bye!")
+                run = False
+
+            if programNumber == 1:
+                fOfx = fOfx.replace(" ", "")
+                slope, yIntercept = fOfx.split("x")
+                if slope == "":
+                    slope = "1"
+                if yIntercept == "":
+                    yIntercept = "0"
+                yIntercept = float(yIntercept)
+                slope = float(slope)
+                print("test")
+                print(f"y = {(slope * xIn) + yIntercept}")
+
+            if programNumber == 2:
+                print(f"y = {(slope1 * xIn) + yIntercept1}")
+
+
+        except:
+            print("test")
+
+
+def pairSort(pair="0,0", programNumber=0, pair1="0,0"):
+    global x
+    global y
+    global x1
+    global y1
+    global pairSuccess
+    try:
+        pairReplace = pair.replace("(", "")
+        pairReplace = pairReplace.replace(")", "")
+        pairReplace = pairReplace.replace(" ", "")
+        x, y = pairReplace.split(",")
+        x = float(x)
+        y = float(y)
+
+        pairReplace = pair1.replace("(", "")
+        pairReplace = pairReplace.replace(")", "")
+        pairReplace = pairReplace.replace(" ", "")
+        x1, y1 = pairReplace.split(",")
+        x1 = float(x1)
+        y1 = float(y1)
+        pairSuccess = True
+
+    except:
+        print("Please type in valid ordered pairs like (2,8) or -4,0")
+        if programNumber == 1:
+            pointSlopeToSlopeIntercept()
+        if programNumber == 2:
+            findSlopeFromPoint()
+        pairSuccess = False
+
+
+def slopeFind(rize="0", run="1"):
+    global slope
+    global success
+    try:
+        rize = float(rize)
+        run = float(run)
+        slope = rize / run
+        success = True
+    except ZeroDivisionError:
+        print("run can't be 0")
+        success = False
+        pointSlopeToSlopeIntercept()
+    except:
+        print("Please type in valid rize and run like rize = 2 and run = 1")
+        pointSlopeToSlopeIntercept()
+        success = False
+
+
+def transformationEquation(gOfx, fOfx):
+    global slope
+    global yIntercept
+    global transformationSuccess
+    try:
+        gOfx = gOfx.replace(" ", "")
+        gOfx = gOfx.replace("(", "")
+        gOfx = gOfx.replace("x", "")
+        gOfx = gOfx.replace("f", "")
+        parenthesesIn, parenthesesOut = gOfx.split(")")
+        if parenthesesIn.__contains__("/"):
+            parenthesesIn = Fraction(parenthesesIn).numerator / Fraction(parenthesesIn).limit_denominator().denominator
+        if parenthesesOut.__contains__("/"):
+            parenthesesOut = Fraction(parenthesesOut).numerator / Fraction(
+                parenthesesOut).limit_denominator().denominator
+
+        fOfx = fOfx.replace(" ", "")
+        slope, yIntercept = fOfx.split("x")
+        if slope == "":
+            slope = "1"
+        if slope.__contains__("/"):
+            slope = Fraction(slope).numerator / Fraction(slope).limit_denominator().denominator
+
+        if yIntercept == "":
+            yIntercept = "0"
+        if yIntercept.__contains__("/"):
+            yIntercept = Fraction(yIntercept).numerator / Fraction(yIntercept).limit_denominator().denominator
+
+        yIntercept = float(yIntercept)
+        slope = float(slope)
+
+        if parenthesesIn.startswith("+") or parenthesesIn.startswith("-"):
+            amount = float(parenthesesIn)
+            yIntercept = (slope * amount) + yIntercept
+            pointSlopeEquation(slope, yIntercept, "g(x)")
+
+        elif parenthesesOut.startswith("+") or parenthesesOut.startswith("-"):
+            amount = float(parenthesesOut)
+            yIntercept = amount + yIntercept
+            pointSlopeEquation(slope, yIntercept, "g(x)")
+
+        elif parenthesesIn.startswith("*"):
+            parenthesesIn = parenthesesIn.replace("*", "")
+            amount = float(parenthesesIn)
+            slope = slope * amount
+            pointSlopeEquation(slope, yIntercept, "g(x)")
+
+        elif parenthesesOut.startswith("*"):
+            parenthesesOut = parenthesesOut.replace("*", "")
+            amount = float(parenthesesOut)
+            slope = slope * amount
+            yIntercept = yIntercept * amount
+            pointSlopeEquation(slope, yIntercept, "g(x)")
+
+        transformationSuccess = True
+    except:
+        print("test")
+
+
+def pointSlopeToSlopeIntercept():
+    # used so the calculations aren't repeated
+    global pairSuccess
+    global success
+    success = False
+    pairSuccess = False
+
+    pair = input("Point = ")
+    pairSort(pair, 1)
+
+    if pairSuccess:
+        rize = input("Rize = ")
+        run = input("Run = ")
+        slopeFind(rize, run, )
+
+    if pairSuccess and success:
+        yIntercept = (-x * slope) + y
+        pointSlopeEquation(slope, yIntercept)
+
+    pairSuccess = True
+
+
+def findSlopeFromPoint(point,point1):
+    global pairSuccess
+    global solved
+    pairSuccess = False
+    pairSort(point, 2, point1, )
+
+    if pairSuccess:
+        findSlopeFromPointEquation()
+        solved = f"The slope is {Slope}"
+        if 1 == 13:
+            # noinspection PyTypeChecker
+            slopeFind(rize, run)
+            yIntercept = (-x * slope) + y
+            pointSlopeEquation(slope, yIntercept)
+
+
+def transformation():
+    global transformationSuccess
+    transformationSuccess = False
+
+    fOfx = input("f(x) = ")
+    gOfx = input("g(x) = ")
+    transformationEquation(gOfx, fOfx)
+    if transformationSuccess and input("Would you like to use x to y converter(yes/no): ") == "yes":
+        xToYConverter(2, slope, yIntercept)
+
+
+def standardToSlopeIntercept():
+    equation = input("Equation in standard form: ")
+    try:
+        equation = equation.replace(" ", "")
+        equation = equation.replace("y", "")
+        left, right = equation.split("=")
+        x, y = left.split("x")
+        if right == "":
+            var = right == 0.0
+
+        y = float(y)
+        yIntercept = float(right) / y
+        slope = -float(x) / y
+        pointSlopeEquation(slope, yIntercept)
+
+    except:
+        print("please type a valid standard form equation like 2x + 3y = 12")
+        standardToSlopeIntercept()
+
+
+def slopeInterceptToStandard():
+    equation = input("y = ")
+    try:
+        equation = equation.replace(" ", "")
+        x, yIntercept = equation.split("x")
+        if yIntercept == "":
+            var = yIntercept == 0.0
+        elif yIntercept.__contains__("/"):
+            yIntercept = Fraction(yIntercept).numerator / Fraction(yIntercept).limit_denominator().denominator
+        else:
+            yIntercept = float(yIntercept)
+        y = 1.0
+
+        if x.__contains__("/"):
+            x = Fraction(x).numerator / Fraction(x).limit_denominator().denominator
+
+        x = -float(x)
+        if not x.is_integer():
+            fraction = Fraction(x)
+            x = fraction.limit_denominator().denominator * x
+            y = fraction.limit_denominator().denominator * y
+            yIntercept = fraction.limit_denominator().denominator * yIntercept
+
+        if not yIntercept.is_integer():
+            fraction = Fraction(yIntercept)
+            x = fraction.limit_denominator().denominator * x
+            y = fraction.limit_denominator().denominator * y
+            yIntercept = fraction.limit_denominator().denominator * yIntercept
+        if x < 0:
+            x = -x
+            y = -y
+            yIntercept = -yIntercept
+        standardEquation(x, y, yIntercept)
+
+    except:
+        print("please type a valid slope intercept form equation like y = 3x +2")
+        slopeInterceptToStandard()
+
--- test.py
+++ test.py
@@ -1,30 +1,44 @@
 import customtkinter
+import GUImath
   # set initial value
-
+test = 1
 
 
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
-        global mode
+
         # Modes: "System" (standard), "Dark", "Light"
         customtkinter.set_appearance_mode("System")
         # Themes: "blue" (standard), "green", "dark-blue"
         customtkinter.set_default_color_theme("blue")
-
+        def run():
+            if combobox.get() == "point slope to slope intercept":
+                GUImath.findSlopeFromPoint(firstPoint.get(), secondPoint.get())
+                answer.configure(text=GUImath.solved)
         def combobox_callback(choice):
-            print("combobox dropdown clicked:", choice)
-            mode = choice
+            if choice == "point slope to slope intercept":
+                firstPoint.grid(row=3, column=0, pady=5)
+                secondPoint.grid(row=4, column=0, pady=5)
+
+            else:
+                firstPoint.grid_remove()
+                secondPoint.grid_remove()
+
         # Window/App Customization
         self.geometry("250x400")
         self.title("Starter Code Example")
-        combobox_var = customtkinter.StringVar(value="option 2")
+        combobox_var = customtkinter.StringVar(value="slope from two points")
         # Grid Configuration
         self.grid_columnconfigure(0, weight=1)
-        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=0)
         self.grid_rowconfigure(1, weight=0)
         self.grid_rowconfigure(2, weight=0)
-        self.grid_rowconfigure(3, weight=1)
+        self.grid_rowconfigure(3, weight=0)
+        self.grid_rowconfigure(4, weight=0)
+        self.grid_rowconfigure(5, weight=0)
+        self.grid_rowconfigure(6, weight=0)
+
 
 
         title = customtkinter.CTkLabel(self,
@@ -32,13 +46,32 @@ class App(customtkinter.CTk):
                                        font=("Arial", 25),
                                        anchor="center")
         combobox = customtkinter.CTkComboBox(master=self,
-                                             values=["option 1", "option 2"],
+                                             values=["slope from two points", "point slope to slope intercept"],
                                              command=combobox_callback,
                                              variable=combobox_var)
-        entry = customtkinter.CTkEntry(self, placeholder_text="CTkEntry")
-        title.grid(row=0, column=0)
-        combobox.grid(row=1,column=0)
-        entry.grid(row=2,column=0)
+        firstPointText = customtkinter.CTkLabel(self,
+                                        text="First Point",
+                                        font=("Arial", 13),
+                                        anchor="w",
+                                        text_color="white")
+
+        firstPoint = customtkinter.CTkEntry(self, placeholder_text="First Point")
+
+        secondPoint = customtkinter.CTkEntry(self, placeholder_text="Second Point")
+
+        runButton = customtkinter.CTkButton(self, text="Run",command=run )
+
+        answer = customtkinter.CTkLabel(self,
+                                        text="",
+                                        font=("Arial", 20),
+                                        anchor="center",
+                                        text_color="grey")
+
+        title.grid(row=0, column=0,pady=10)
+        combobox.grid(row=1,column=0,pady=20)
+        firstPointText.grid(row=2, column=0,sticky="w",padx=60)
+        runButton.grid(row=5,column=0,pady=20)
+        answer.grid(row=6, column=0,pady=10,sticky="nsew")
 
 app = App()
 app.mainloop()
