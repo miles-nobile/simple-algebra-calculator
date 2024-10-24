f7b455f06b33c7d6b96cd130f5f0eee6b6807bee
msmn <msmn@sungrovemontessori.org>
2024-10-24 14:27:26 -0400

    solve

--- solve.py
+++ solve.py
@@ -1,12 +1,24 @@
 x = -1
-test = "2*(2+3)/1"
+test = "2+2+2"
 test = test.replace(" ","")
 test = test.replace("+","s+")
 test = test.replace("-","s-")
 test = test.replace("*","a*")
 test = test.replace("/","a/")
+
+test1 = test.split("(")
+test2 = []
+y = -1
+for test3 in test1:
+    y = y + 1
+    test2.extend(test3.split(")"))
+
+print(test2)
+
 symple = test.split("s")
+
 print(symple)
+print()
 id = []
 solve = 1.0
 for part in symple:
@@ -28,6 +40,39 @@ for part in symple:
 print(id)
 answer = 0
 for part in id:
-    float()
     answer = answer + float(part)
 print(answer)
+
+def pemdas(equation):
+    x = -1
+    id = []
+    equation = equation.replace(" ", "")
+    equation = equation.replace("+", "s+")
+    equation = equation.replace("-", "s-")
+    equation = equation.replace("*", "a*")
+    equation = equation.replace("/", "a/")
+
+    for part in symple:
+        x = x + 1
+        if part.__contains__("a"):
+            part = part.split("a")
+            for piece in part:
+                if not piece.__contains__("*") and not piece.__contains__("/"):
+                    solve = float(piece)
+                elif piece.__contains__("*"):
+                    piece = piece.replace("*", "")
+                    solve = solve * float(piece)
+                elif piece.__contains__("/"):
+                    piece = piece.replace("/", "")
+                    solve = solve / float(piece)
+            id.insert(x, solve)
+        else:
+            id.insert(x, part)
+    print(id)
+    answer = 0
+    for part in id:
+        float()
+        answer = answer + float(part)
+    return answer
+
+print(pemdas("2+2+2"))
