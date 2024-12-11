4b408238586808bdcba083e92a2413bbcaf0f78c
msmn <msmn@sungrovemontessori.org>
2024-12-11 11:40:39 -0500

    pemdas

--- solvemMod.py
+++ solvemMod.py
@@ -2,12 +2,12 @@
 
 def emdas(equation):
     id = []
-    symple = equation.split("s")
+    symple = equation.split("ʂ")
     x = -1
     for part in symple:
         x = x + 1
-        if part.__contains__("a"):
-            part = part.split("a")
+        if part.__contains__("ą"):
+            part = part.split("ą")
             for piece in part:
                 if not piece.__contains__("*") and not piece.__contains__("/"):
                     if piece.__contains__("^"):
@@ -59,38 +59,38 @@ def pemdas(equation):
     half = ""
     x = -1
     equation = equation.replace(" ", "")
-    equation = equation.replace("+", "s+")
-    equation = equation.replace("-", "s-")
-    equation = equation.replace("*", "a*")
-    equation = equation.replace("/", "a/")
-    equation = equation.replace("0(", "0a*(")
-    equation = equation.replace("1(", "1a*(")
-    equation = equation.replace("2(", "2a*(")
-    equation = equation.replace("3(", "3a*(")
-    equation = equation.replace("4(", "4a*(")
-    equation = equation.replace("5(", "5a*(")
-    equation = equation.replace("6(", "6a*(")
-    equation = equation.replace("7(", "7a*(")
-    equation = equation.replace("8(", "8a*(")
-    equation = equation.replace("9(", "9a*(")
-    equation = equation.replace(")0", "p)a*0")
-    equation = equation.replace(")1", "p)a*1")
-    equation = equation.replace(")2", "p)a*2")
-    equation = equation.replace(")3", "p)a*3")
-    equation = equation.replace(")4", "p)a*4")
-    equation = equation.replace(")5", "p)a*5")
-    equation = equation.replace(")6", "p)a*6")
-    equation = equation.replace(")7", "p)a*7")
-    equation = equation.replace(")8", "p)a*8")
-    equation = equation.replace(")9", "p)a*9")
+    equation = equation.replace("+", "ʂ+")
+    equation = equation.replace("-", "ʂ-")
+    equation = equation.replace("*", "ą*")
+    equation = equation.replace("/", "ą/")
+    equation = equation.replace("0(", "0ą*(")
+    equation = equation.replace("1(", "1ą*(")
+    equation = equation.replace("2(", "2ą*(")
+    equation = equation.replace("3(", "3ą*(")
+    equation = equation.replace("4(", "4ą*(")
+    equation = equation.replace("5(", "5ą*(")
+    equation = equation.replace("6(", "6ą*(")
+    equation = equation.replace("7(", "7ą*(")
+    equation = equation.replace("8(", "8ą*(")
+    equation = equation.replace("9(", "9ą*(")
+    equation = equation.replace(")0", "p)ą*0")
+    equation = equation.replace(")1", "p)ą*1")
+    equation = equation.replace(")2", "p)ą*2")
+    equation = equation.replace(")3", "p)ą*3")
+    equation = equation.replace(")4", "p)ą*4")
+    equation = equation.replace(")5", "p)ą*5")
+    equation = equation.replace(")6", "p)ą*6")
+    equation = equation.replace(")7", "p)ą*7")
+    equation = equation.replace(")8", "p)ą*8")
+    equation = equation.replace(")9", "p)ą*9")
     equation = equation.replace(")", "p)")
-    equation = equation.replace("+a*", "+")
-    equation = equation.replace("-a*", "-")
-    equation = equation.replace("a*s+", "s+")
-    equation = equation.replace("a*s-", "s-")
-    equation = equation.replace("/a*", "/")
-    equation = equation.replace("*a/", "/")
-    equation = equation.replace("a*a*", "a*")
+    equation = equation.replace("+ą*", "+")
+    equation = equation.replace("-ą*", "-")
+    equation = equation.replace("ą*ʂ+", "ʂ+")
+    equation = equation.replace("ą*ʂ-", "ʂ-")
+    equation = equation.replace("/ą*", "/")
+    equation = equation.replace("*ą/", "/")
+    equation = equation.replace("ą*ą*", "ą*")
     parentheses = equation.split("(")
     for part in parentheses:
         solves = ""
@@ -107,4 +107,4 @@ def pemdas(equation):
 
     return str(emdas(half))
 
-print(pemdas(input("Equation:\n")))
+print(pemdas("1+3+(7*3)"))
