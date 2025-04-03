3f3e5a6292bce8e5393147aafcb87e1870caa15b
I-can-code-too <milesnobile@gmail.com>
2025-04-02 20:00:54 -0400

    test

--- solvemMod.py
+++ solvemMod.py
@@ -5,5 +5,14 @@ def factors(n):
         list.__add__,
         ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
 
-def factor_trinomials(equation):
-    pass
+print(list(factors(20)))
+def factor_trinomials(equation,equation1):
+    factor_list = list(factors(equation))
+    while len(factor_list) >= 1:
+        if float(equation1) == float(factor_list[0])+ float(factor_list[-1]):
+            return f"{factor_list[0]}, {factor_list[-1]}"
+        else:
+            pass
+
+
+print(factor_trinomials(12,13))
