ed9a832021ee93c3a21bec889330ab60aaa8097e
msmn <msmn@sungrovemontessori.org>
2025-04-03 11:21:26 -0400

    factor trinomials

--- solvemMod.py
+++ solvemMod.py
@@ -1,4 +1,9 @@
 from functools import reduce
+from fractions import Fraction
+
+
+def denomator(x): return Fraction(x).limit_denominator().denominator
+def numerator(x): return Fraction(x).limit_denominator().numerator
 
 
 def find_gcf_two_numbers(a, b):
@@ -7,7 +12,7 @@ def find_gcf_two_numbers(a, b):
     return a
 
 
-def find_gcf_three_numbers(a, b, c):
+def gcf_three_numbers(a, b, c):
 
     gcf_ab = find_gcf_two_numbers(a, b)
 
@@ -20,19 +25,31 @@ def factors(n):
         list.__add__,
         ([i, n//i] for i in range(1, int(n**0.5) + 1) if n % i == 0)))
 
-def factor(equation,equation1,num0):
+def factor(equation,equation1,num0, gcf):
     factor_list = list(factors(abs(equation)))
-    while len(factor_list) > 0:
-        if float(equation1) == float(factor_list[0])+ float(factor_list[-1]):
-            return f"{int(factor_list[0])}, {int(factor_list[-1])}"
-        elif float(equation1) == - float(factor_list[0]) - float(factor_list[-1]):
-            return f"-{factor_list[0]}, -{factor_list[-1]}"
-        elif float(equation1) == - float(factor_list[0]) + float(factor_list[-1]):
-            return f"-{factor_list[0]}, {factor_list[-1]}"
-        elif float(equation1) == + float(factor_list[0]) - float(factor_list[-1]):
-            return f"{factor_list[0]}, -{factor_list[-1]}"
+    while len(factor_list) > 1:
+        if gcf > 1:
+            if float(equation1) == float(factor_list[0])+ float(factor_list[-1]):
+                return f"{gcf}({numerator(float(num0)/float(factor_list[0]))}x + {denomator(float(num0)/float(factor_list[0]))})({numerator(float(num0)/float(factor_list[-1]))}x + {denomator(float(num0)/float(factor_list[-1]))})"
+            elif float(equation1) == - float(factor_list[0]) - float(factor_list[-1]):
+                return f"{gcf}({numerator(float(num0)/float(factor_list[0]))}x - {denomator(float(num0)/float(factor_list[0]))})({numerator(float(num0)/float(factor_list[-1]))}x - {denomator(float(num0)/float(factor_list[-1]))})"
+            elif float(equation1) == - float(factor_list[0]) + float(factor_list[-1]):
+                return f"{gcf}({numerator(float(num0)/float(factor_list[0]))}x - {denomator(float(num0)/float(factor_list[0]))})({numerator(float(num0)/float(factor_list[-1]))}x + {denomator(float(num0)/float(factor_list[-1]))})"
+            elif float(equation1) == + float(factor_list[0]) - float(factor_list[-1]):
+                return f"{gcf}({numerator(float(num0)/float(factor_list[0]))}x + {denomator(float(num0)/float(factor_list[0]))})({numerator(float(num0)/float(factor_list[-1]))}x - {denomator(float(num0)/float(factor_list[-1]))})"
+            else:
+                pass
         else:
-            pass
+            if float(equation1) == float(factor_list[0])+ float(factor_list[-1]):
+                return f"({numerator(float(num0)/float(factor_list[0]))}x + {denomator(float(num0)/float(factor_list[0]))})({numerator(float(num0)/float(factor_list[-1]))}x + {denomator(float(num0)/float(factor_list[-1]))})"
+            elif float(equation1) == - float(factor_list[0]) - float(factor_list[-1]):
+                return f"({numerator(float(num0)/float(factor_list[0]))}x - {denomator(float(num0)/float(factor_list[0]))})({numerator(float(num0)/float(factor_list[-1]))}x - {denomator(float(num0)/float(factor_list[-1]))})"
+            elif float(equation1) == - float(factor_list[0]) + float(factor_list[-1]):
+                return f"({numerator(float(num0)/float(factor_list[0]))}x - {denomator(float(num0)/float(factor_list[0]))})({numerator(float(num0)/float(factor_list[-1]))}x + {denomator(float(num0)/float(factor_list[-1]))})"
+            elif float(equation1) == + float(factor_list[0]) - float(factor_list[-1]):
+                return f"({numerator(float(num0)/float(factor_list[0]))}x + {denomator(float(num0)/float(factor_list[0]))})({numerator(float(num0)/float(factor_list[-1]))}x - {denomator(float(num0)/float(factor_list[-1]))})"
+            else:
+                pass
         del factor_list[0]
         del factor_list[-1]
     return "Prime"
@@ -52,12 +69,17 @@ def factor_trinomials(equation):
     equation = equation.replace("8-", "s-")
     equation = equation.replace("9-", "s-")
     equation = equation.replace("x-", "s-")
+    equation = equation.replace("^2", "")
+    equation = equation.replace("x", "")
     num0, num1, num2 = equation.split("s")
+    gcf = gcf_three_numbers(int(num0),int(num1),int(num2))
+    num0 = int(num0) / gcf
+    num1 = int(num1) / gcf
+    num2 = int(num2) / gcf
     test = float(num0) * float(num2)
-    print(float(num1))
-    print(test)
-    return factor(test, num1,num0)
+    test = int(test)
+    return factor(test, num1, num0, gcf)
 
 
-print(factor_trinomials("-2+4+6"))
+print(factor_trinomials("2x^2+8x+6"))
 
